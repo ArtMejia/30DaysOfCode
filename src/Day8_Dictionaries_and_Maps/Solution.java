e30179c1a6052f11aeab574db737f1a19a905ef4
ArtMejia <arteniomejia.student@careerdevs.com>
2022-10-06 19:35:26 -0400

    30 Days Of Code

--- src/Day8_Dictionaries_and_Maps/Solution.java
+++ src/Day8_Dictionaries_and_Maps/Solution.java
@@ -7,19 +7,19 @@ class Solution{
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        Map<String, Integer> nPhone = new HashMap<String, Integer>();
+        Map<String, Integer> contact = new HashMap<String, Integer>();
         for (int i = 0; i < n; i++) {
             String name = in.next();
             int phone = in.nextInt();
-            nPhone.put(name, phone);
+            contact.put(name, phone);
         }
         while (in.hasNext()) {
             String s = in.next();
-            if (nPhone.get(s) == null) {
+            if (contact.get(s) == null) {
                 System.out.println("Not found");
             }
             else {
-                System.out.println(s + "=" + nPhone.get(s));
+                System.out.println(s + "=" + contact.get(s));
             }
         }
         in.close();

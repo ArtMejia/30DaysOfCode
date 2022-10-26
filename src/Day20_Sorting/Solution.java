255588bc5a2bdd2e9c2e52b168b23e9f8faca8c1
ArtMejia <arteniomejia.student@careerdevs.com>
2022-10-25 21:00:49 -0400

    30 Days Of Code

---
+++ src/Day20_Sorting/Solution.java
@@ -0,0 +1,42 @@
+package Day20_Sorting;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        // Write your code here
+        int count =0;
+
+        for (int i = 0; i < n; i++) {
+            int numberOfSwaps = 0;
+
+            for (int j = 0; j < n - 1; j++) {
+                if (a.get(j) > a.get(j+1)) {
+                    Collections.swap(a,j ,j+1);
+                    numberOfSwaps++;
+                }
+            }
+            count += numberOfSwaps;
+            if (numberOfSwaps == 0) {
+                break;
+            }
+        }
+
+        System.out.println("Array is sorted in "+count+" swaps.");
+        System.out.println("First Element: "+a.get(0));
+        System.out.println("Last Element: "+a.get(a.size()-1));
+
+        bufferedReader.close();
+    }
+}

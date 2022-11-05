d64b7031c10b320382d8f9a3d2c27a3580c39c22
ArtMejia <arteniomejia.student@careerdevs.com>
2022-11-05 19:24:15 -0400

    Complete Day 24

--- src/Day24_More_Linked_List/Solution.java
+++ src/Day24_More_Linked_List/Solution.java
@@ -16,8 +16,18 @@ class Solution
 
     public static Node removeDuplicates(Node head) {
         //Write your code here
-        Node blank = head;
-        return blank;
+        if (head == null) {
+            return null;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            if (temp.data != temp.next.data) {
+                temp = temp.next;
+            } else {
+                temp.next = temp.next.next;
+            }
+        }
+        return head;
     }
 
     public static  Node insert(Node head,int data)

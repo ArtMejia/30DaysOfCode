d0d424343c219213b83a68e15551ed9e6371c3b0
ArtMejia <arteniomejia.student@careerdevs.com>
2022-11-11 14:26:23 -0500

    Completed Day 25

--- src/Day25_Running_Time_and_Complexity/Solution.java
+++ src/Day25_Running_Time_and_Complexity/Solution.java
@@ -6,6 +6,24 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int T = in.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int n = in.nextInt();
+
+            if (n >= 2 && isPrime(n))
+                System.out.println("Prime");
+            else
+                System.out.println("Not Prime");
+        }
+    }
+    public static boolean isPrime(int n) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if(n%i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
